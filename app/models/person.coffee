f8f5c1b62668b2dd2cea4a5337d92696dd2444f9
patricksimpson <izerop@gmail.com>
2014-02-14 07:07:23 -0500

    person page WIP

--- app/models/person.coffee
+++ app/models/person.coffee
@@ -1,6 +1,7 @@
 App.Person = FP.Model.extend(
   name: FP.attr 'string'
   email: FP.attr 'string'
+  games: FP.hasMany("games", {embedded: false, as: "games"})
 )
 
 ###
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -207,7 +207,11 @@ module.exports = Ember.Application.initializer({
 ;require.register("models/person", function(exports, require, module) {
 App.Person = FP.Model.extend({
   name: FP.attr('string'),
-  email: FP.attr('string')
+  email: FP.attr('string'),
+  games: FP.hasMany("games", {
+    embedded: false,
+    as: "games"
+  })
 });
 
 /*
@@ -344,14 +348,37 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 module.exports = Ember.TEMPLATES['person'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, hashTypes, hashContexts, escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n    <p>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "game.score", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</p>\n  ");
+  return buffer;
+  }
 
-  data.buffer.push("<h2>");
+  data.buffer.push("<div class=\"profile\">\n  <h2>");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "name", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("</h2>\n");
+  data.buffer.push("</h2>\n  <a href=\"#\">Challenge</a>\n  <div class=\"stats\">\n    Wins: <span class=\"wins\">");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "wins", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</span>\n    Losses: <span class=\"losses\">");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "losses", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</span>\n  </div>\n  ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers.each.call(depth0, "games", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n</div>\n");
   return buffer;
   
 });
