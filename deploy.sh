2e83de81b24b0d9ffc6a2f35f3ce4dd14984189d
patricksimpson <izerop@gmail.com>
2014-03-03 20:33:57 -0500

    fixing deploy script, adding fonts back in

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 cake build
-git checkout gh-pages
-ls -1 | grep -v public | grep -v node_modules | grep -v deploy.sh | xargs rm -rf
-mv public/* .
+cd ../table-deploy
+ls -1 | xargs rm -rf
+mv ../table/public/* .
 git add -A
-git commit -m -a "deploy"
+git commit -m "deploy"
 git push
 echo "Done deploying!"
