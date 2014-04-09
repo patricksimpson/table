bfad932c70eb6502404d923e9e9dbf5f89633509
patricksimpson <izerop@gmail.com>
2014-04-09 19:23:05 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
-#!/bin/zsh
-echo "building production"
-cake build
-echo "moving files to deploy directory..."
-cd ../table-deploy
-ls -1 | xargs rm -rf
-mv ../table/public/* .
-echo "thetable.io" > CNAME
-git add -A
-git commit -m "deploy"
-git push
-cd ../table
-echo "Done deploying!"
+Uncommitted changes, continue? (yN) Aborting.
